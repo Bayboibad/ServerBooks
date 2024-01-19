ea75151251ebb0e9bfca3dd98658b11b93bcc56c
CuongBom <cuongezs280667@gmail.com>
2024-01-19 21:02:12 +0700

    create api user

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user.model');
 
-/* GET home page. */
+var userCtrl = require("../controllers/user.controller");
+
+router.get('/list', userCtrl.listUser);
+router.post('/addUser', userCtrl.addUser);
+router.post('/updateUser', userCtrl.updateUser);
+router.delete('/deleteUser/:_id', userCtrl.deleteUser);
+router.get('/deleteUserOnWeb', userCtrl.deleteUserOnWeb);
+
 router.get('/',async function(req, res, next) {
-  const admin = req.session.admin; // Lấy thông tin người dùng từ session
+  const admin = req.session.admin;
+  const dataUser = await User.find()
   if(admin){
     console.log(admin)
-    res.render('user', {title:'user'});
+    res.render('user', {dataUser:dataUser});
   }else{
     console.log('No librarian in session');
-    res.redirect('/');
+    res.redirect('/'); 
   }
 });
 module.exports = router;
